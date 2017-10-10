33c8ffd5729aeec91e867aa1f7c3f16676918d2b
Yang LI <zjzsliyang@gmail.com>
2017-10-10 10:41:00 +0800

    fix bugs

--- hw1/1452669-hw1-q3.sh
+++ hw1/1452669-hw1-q3.sh
@@ -8,9 +8,9 @@ exec 2>>$log
 # $4: student ID
 ssh $1@$2 "/bin/bash $3/$4-hw1-q1.sh"
 scp ./$4-hw1-q1.log $1@$2:$4-hw1-q1.log
-b_start_time=$(awk '(NR==1) {print $1}' "$(pwd)/$4-hw1-q1.log")
-a_start_time=$(awk '(NR==1) {print $1}' "$(pwd)/1452669-hw1-q1.log")
-b_start_time_stamp=$(date +%s -d $b_start_time)
-a_start_time_stamp=$(date +%s -d $a_start_time)
+b_start_time=$(ls -lrt | grep $4-hw1-q1.log | cut -d ' ' -f 8-10)
+a_start_time=$(ls -lrt | grep 1452669-hw1-q1.log | cut -d ' ' -f 8-10)
+b_start_time_stamp=$(date +%s -d "$b_start_time")
+a_start_time_stamp=$(date +%s -d "$a_start_time")
 interval_time=$(($b_start_time_stamp - $a_start_time_stamp))
 echo $interval_time >>$log
