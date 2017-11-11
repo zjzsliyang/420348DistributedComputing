a316d926e95aaeb002bdcf4bd50ad698cebdeead
Yang LI <zjzsliyang@gmail.com>
2017-11-12 04:11:31 +0800

    Add producer

--- hw2-HDFS/src/main/java/Producer.java
+++ hw2-HDFS/src/main/java/Producer.java
@@ -1,24 +1,56 @@
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.IntBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileChannel.MapMode;
 
-public class Producer {
+public class Producer implements Runnable {
+  private static final String OUTPUT_PATH = "data_generation.txt";
+  private static final int MAX_NUM = 2014 * 512;
+  private static final short TIMES = 256;
+  private static final int TOTAL = 2014 * 512 * 256;
 
-  public static void producer(int range, int times, String fileName)
-      throws FileNotFoundException, UnsupportedEncodingException {
-    PrintWriter writer = null;
+  @Override
+  public void run() {
+    long costTime = -1;
+    long startTime = System.currentTimeMillis();
+
+    RandomAccessFile os = null;
+    FileChannel fo = null;
     try {
-      writer = new PrintWriter(fileName, "UTF-8");
-      for (int i = 0; i < range; i++) {
-        for (int j = 0; j < times; j++) {
-          writer.println(i + 1);
+      os = new RandomAccessFile(OUTPUT_PATH, "rw");
+      fo = os.getChannel();
+      try {
+        IntBuffer oIb = fo.map(MapMode.READ_WRITE, 0, TOTAL * 1000).asIntBuffer();
+        for (int i = 0; i < MAX_NUM; i++) {
+          for (int j = 0; j < TIMES; j++) {
+            oIb.put(i);
+          }
         }
+      } catch (IOException e) {
+        e.printStackTrace();
       }
-      writer.close();
     } catch (FileNotFoundException e) {
-      throw e;
-    } catch (UnsupportedEncodingException e) {
-      throw e;
+      e.printStackTrace();
+    } finally {
+      try {
+        if (fo != null) {
+          fo.close();
+        }
+        if (os != null) {
+          os.close();
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
+
+    long endTime = System.currentTimeMillis();
+    costTime = endTime - startTime;
+    System.out.println(costTime);
   }
 }
