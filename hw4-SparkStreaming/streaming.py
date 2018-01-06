c69425022f680263c5dff009ca591fd3aa9f56e1
Yang LI <zjzsliyang@gmail.com>
2018-01-07 06:17:53 +0800

    add hw4

--- hw4-SparkStreaming/streaming.py
+++ hw4-SparkStreaming/streaming.py
@@ -2,15 +2,17 @@ from string import punctuation
 from operator import add, itemgetter
 
 from pyspark import SparkConf, SparkContext
-from pyspark.sql import SQLContext
+from pyspark.sql import SQLContext, SparkSession
 from pyspark.sql.types import *
+from pyspark.sql.functions import *
 from pyspark.streaming import *
+import re
 
 conf = (SparkConf()
-        .setMaster('local[2]')
-        .setAppName('distcomp-h4')
-        .set('spark.jars.packages', 'com.databricks:spark-xml_2.11:0.4.1')
-        .set('spark.executor.memory', '4g'))
+    .setMaster('local[*]')
+    .setAppName('distcomp-h4')
+    .set('spark.jars.packages', 'com.databricks:spark-xml_2.11:0.4.1')
+    .set('spark.executor.memory', '4g'))
 sc = SparkContext(conf=conf)
 sqlContext = SQLContext(sc)
 streamingContext = StreamingContext(sc, 5)
@@ -24,55 +26,70 @@ schema = StructType([
 def handle(instruction):
     if instruction.lower().startswith('author: '):
         print('case 1')
+        author = instruction[8:]
+        return author
     elif instruction.lower().startswith('coauthor: '):
         print('case 2')
+        author = instruction[10:]
+        return author
     elif instruction.lower().startswith('clique: '):
         print('case 3')
+        author = re.split(',[ ]*', instruction[8:-3])
+        return author, instruction[-1]
+    return ""
 
 
 def main():
+    # TODO: case insensitive: .lower()
+    # TODO: get context instance
     lines = streamingContext.socketTextStream('localhost', 9999)
     lines = lines.flatMap(lambda x: x.split('\n'))
-    lines.foreachRDD(lambda rdd: rdd.foreach(handle))
 
     article = sqlContext.read.format('com.databricks.spark.xml') \
         .options(rowTag='article', excludeAttribute=True, charset='utf-8') \
-        .load('dblp_converted.xml', schema=schema)
+        .load('data/dblp_converted.xml', schema=schema)
+
+    inproceedings = sqlContext.read.format('com.databricks.spark.xml') \
+        .options(rowTag='inproceedings', excludeAttribute=True, charset='utf-8') \
+        .load('data/dblp_converted.xml', schema=schema)
+
+    pvldb = article.filter(article.url.contains('journals/acta'))
+    sigmod = inproceedings.filter(inproceedings.url.contains('conf/sigmod/'))
+    icde = inproceedings.filter(inproceedings.url.contains('conf/icde/'))
+
+    getArticleNo = lambda author: pvldb.select('author').rdd.map(lambda x: x[0]).map(
+        lambda x: 1 if author in x else 0).reduce(add)
+    getArticleTitle = lambda author: pvldb.filter(array_contains('author', author)).orderBy(pvldb.year.desc()).select(
+        'title').rdd.map(lambda x: x[0]).collect()
+    print(getArticleNo('Junhu Wang'))
+    print(getArticleTitle('Nathan Goodman'))
+
+    getCoNo = lambda author: pvldb.filter(array_contains('author', author)).select('author').rdd.map(
+        lambda x: len(x[0]) - 1).collect()
+    getCoArticle = lambda author: pvldb.filter(array_contains('author', author)).select('author').rdd.map(
+        lambda x: x[0]).flatMap(lambda authors: [(x, 1) for x in authors]).reduceByKey(add).sortBy(lambda x: -x[1]) \
+        .collect()
+    print(getCoNo('Nathan Goodman'))
+    print(getCoArticle('Nathan Goodman'))
+
+    def filters(df, au):
+        for item in au:
+            df = df.filter(item)
+        return df
+
+    getClique = lambda aus, no: 'YES' if filters(pvldb,
+                                                 [array_contains('author', x) for x in aus]).count() > no else 'NO'
+    getCliqueArticle = lambda aus: filters(pvldb, [array_contains('author', x) for x in aus]).orderBy(
+        pvldb.year.desc()).select('title', 'author', 'year').rdd.collect()
+    print(getClique(['Nathan Goodman', 'Christian Lengauer'], 5))
+    print(getCliqueArticle(['Nathan Goodman', 'Christian Lengauer']))
+
+    articleNo = lines.map(handle)
+    articleNo.pprint()
 
     streamingContext.start()
     streamingContext.awaitTermination()
 
-    #
-    #
-    #
-    #
-    #
-    #
-    # article = sqlContext.read.format('com.databricks.spark.xml') \
-    #     .options(rowTag='article', excludeAttribute=True, charset='utf-8') \
-    #     .load('dblp_converted.xml', schema=schema)
-    # article = article.filter(article.year >= 2000)
-    #
-    # inproceedings = sqlContext.read.format('com.databricks.spark.xml') \
-    #     .options(rowTag='inproceedings', excludeAttribute=True, charset='utf-8') \
-    #     .load('dblp_converted.xml', schema=schema)
-    # inproceedings = inproceedings.filter(inproceedings.year >= 2000)
-    #
-    # pvldb = article.filter(article.url.contains('journals/pvldb/')).select('title')
-    # sigmod = inproceedings.filter(inproceedings.url.contains('conf/sigmod/')).select('title')
-    # icde = inproceedings.filter(inproceedings.url.contains('conf/icde/')).select('title')
-    #
-    # translator = str.maketrans('', '', punctuation)  # strip punctuation marks from a string
-    # titles = pvldb.union(sigmod).union(icde)
-    # keywords = titles.rdd.flatMap(lambda x: x[0].translate(translator).split()) \
-    #     .map(lambda x: x.lower()).filter(lambda x: x not in stopwords) \
-    #     .map(lambda x: (x, 1)).reduceByKey(add) \
-    #     .sortBy(itemgetter(1), ascending=False).take(100)
-    #
-    # with open('hw3-1552699-db-top100keywords.txt', 'w') as f:
-    #     for keyword in keywords:
-    #         f.write('{}: {}\n'.format(keyword[0], keyword[1]))
-    #
 
 if __name__ == '__main__':
     main()
