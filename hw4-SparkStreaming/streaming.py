881c3343f10c1f7432e79c705205f18cee169120
Yang LI <zjzsliyang@gmail.com>
2018-01-15 10:42:07 +0800

    complete hw4

--- hw4-SparkStreaming/streaming.py
+++ hw4-SparkStreaming/streaming.py
@@ -1,21 +1,11 @@
-from string import punctuation
-from operator import add, itemgetter
+from operator import add
 
-from pyspark import SparkConf, SparkContext
-from pyspark.sql import SQLContext, SparkSession
-from pyspark.sql.types import *
-from pyspark.sql.functions import *
-from pyspark.streaming import *
 import re
+from pyspark import SparkConf, SparkContext, SQLContext
+from pyspark.sql.functions import udf
+from pyspark.sql.types import StructType, StructField, StringType, ArrayType, IntegerType, BooleanType
+from pyspark.streaming import StreamingContext
 
-conf = (SparkConf()
-    .setMaster('local[*]')
-    .setAppName('distcomp-h4')
-    .set('spark.jars.packages', 'com.databricks:spark-xml_2.11:0.4.1')
-    .set('spark.executor.memory', '4g'))
-sc = SparkContext(conf=conf)
-sqlContext = SQLContext(sc)
-streamingContext = StreamingContext(sc, 5)
 schema = StructType([
     StructField('title', StringType(), True),
     StructField('author', ArrayType(StringType()), True),
@@ -23,72 +13,94 @@ schema = StructType([
     StructField('url', StringType(), True)])
 
 
-def handle(instruction):
-    if instruction.lower().startswith('author: '):
-        print('case 1')
-        author = instruction[8:]
-        return author
-    elif instruction.lower().startswith('coauthor: '):
-        print('case 2')
-        author = instruction[10:]
-        return author
-    elif instruction.lower().startswith('clique: '):
-        print('case 3')
-        author = re.split(',[ ]*', instruction[8:-3])
-        return author, instruction[-1]
-    return ""
+def case_insensitive_array_contains(name, names_arr):
+    lower_name = name.lower()
+    if names_arr is not None:
+        for names_ in names_arr:
+            if lower_name in names_.lower():
+                return True
+    return False
+
+
+def case_insensitive_arrays_contains(names, names_arr):
+    cnt = 0
+    flag = False
+    for name in names:
+        lower_name = name.lower()
+        if names_arr is not None:
+            for name_ in names_arr:
+                if lower_name in name_.lower():
+                    flag = True
+        if flag is True:
+            cnt += 1
+            flag = False
+    if cnt == len(names):
+        return True
+    else:
+        return False
 
 
 def main():
-    # TODO: case insensitive: .lower()
-    # TODO: get context instance
-    lines = streamingContext.socketTextStream('localhost', 9999)
-    lines = lines.flatMap(lambda x: x.split('\n'))
+    conf = (SparkConf()
+        .setMaster('local[*]')
+        .setAppName('distcomp-h4')
+        .set('spark.jars.packages', 'com.databricks:spark-xml_2.11:0.4.1')
+        .set('spark.executor.memory', '4g'))
+    sc = SparkContext(conf=conf)
+    streaming_context = StreamingContext(sc, 5)
+    lines = streaming_context.socketTextStream('localhost', 9999)
 
-    article = sqlContext.read.format('com.databricks.spark.xml') \
-        .options(rowTag='article', excludeAttribute=True, charset='utf-8') \
-        .load('data/dblp_converted.xml', schema=schema)
+    def handle(rdd):
+        input = rdd.collect()
+        scc = SparkContext.getOrCreate(conf)
+        sql = SQLContext(scc)
 
-    inproceedings = sqlContext.read.format('com.databricks.spark.xml') \
-        .options(rowTag='inproceedings', excludeAttribute=True, charset='utf-8') \
-        .load('data/dblp_converted.xml', schema=schema)
+        udf(case_insensitive_array_contains, BooleanType())
+        udf(case_insensitive_arrays_contains, BooleanType())
+        article = sql.read.format('com.databricks.spark.xml') \
+            .options(rowTag='article', excludeAttribute=True, charset='utf-8') \
+            .load('data/dblp_converted.xml', schema=schema)
 
-    pvldb = article.filter(article.url.contains('journals/acta'))
-    sigmod = inproceedings.filter(inproceedings.url.contains('conf/sigmod/'))
-    icde = inproceedings.filter(inproceedings.url.contains('conf/icde/'))
+        output = []
+        if len(input) > 0:
+            if input[0].lower().startswith('author: '):
+                # case 1:
+                author = input[0][8:]
+                print(article.rdd.filter(lambda x: case_insensitive_array_contains(author, x['author'])).map(
+                    lambda x: x['title']).collect().__len__())
+                output = article.orderBy(article.year.desc()).rdd.filter(
+                    lambda x: case_insensitive_array_contains(author, x['author'])).map(
+                    lambda x: x['title']).collect()
+                for item in output:
+                    print(item)
+            elif input[0].lower().startswith('coauthor: '):
+                # case 2:
+                author = input[0][10:]
+                print(
+                    article.select('author').rdd.filter(lambda x: case_insensitive_array_contains(author, x['author'])).map(
+                        lambda x: x['author']).flatMap(lambda x: x).distinct().count() - 1)
+                output = article.select('author').rdd.filter(
+                    lambda x: case_insensitive_array_contains(author, x['author'])).map(
+                    lambda x: x['author']).flatMap(lambda au: [(x, 1) for x in au]).reduceByKey(add).sortBy(
+                    lambda x: -x[1]).collect()
+                output.pop(0)
+                for item in output:
+                    print(item)
 
-    getArticleNo = lambda author: pvldb.select('author').rdd.map(lambda x: x[0]).map(
-        lambda x: 1 if author in x else 0).reduce(add)
-    getArticleTitle = lambda author: pvldb.filter(array_contains('author', author)).orderBy(pvldb.year.desc()).select(
-        'title').rdd.map(lambda x: x[0]).collect()
-    print(getArticleNo('Junhu Wang'))
-    print(getArticleTitle('Nathan Goodman'))
+            elif input[0].lower().startswith('clique: '):
+                # case 3:
+                author = re.split(',[ ]*', re.split('[\[\]]', input[0])[0][8:])
+                no = int(re.split('[\[\]]', input[0])[-2])
+                print('YES' if article.rdd.filter(lambda x: case_insensitive_arrays_contains(author, x['author'])).count() >= no else 'NO')
+                output = article.rdd.filter(lambda x: case_insensitive_arrays_contains(author, x['author'])).map(lambda x: [x[i] for i in ['title', 'author', 'year']]).collect()
+                for item in output:
+                    print(item)
+            else:
+                print('illegal input pattern.')
 
-    getCoNo = lambda author: pvldb.filter(array_contains('author', author)).select('author').rdd.map(
-        lambda x: len(x[0]) - 1).collect()
-    getCoArticle = lambda author: pvldb.filter(array_contains('author', author)).select('author').rdd.map(
-        lambda x: x[0]).flatMap(lambda authors: [(x, 1) for x in authors]).reduceByKey(add).sortBy(lambda x: -x[1]) \
-        .collect()
-    print(getCoNo('Nathan Goodman'))
-    print(getCoArticle('Nathan Goodman'))
-
-    def filters(df, au):
-        for item in au:
-            df = df.filter(item)
-        return df
-
-    getClique = lambda aus, no: 'YES' if filters(pvldb,
-                                                 [array_contains('author', x) for x in aus]).count() > no else 'NO'
-    getCliqueArticle = lambda aus: filters(pvldb, [array_contains('author', x) for x in aus]).orderBy(
-        pvldb.year.desc()).select('title', 'author', 'year').rdd.collect()
-    print(getClique(['Nathan Goodman', 'Christian Lengauer'], 5))
-    print(getCliqueArticle(['Nathan Goodman', 'Christian Lengauer']))
-
-    articleNo = lines.map(handle)
-    articleNo.pprint()
-
-    streamingContext.start()
-    streamingContext.awaitTermination()
+    lines.foreachRDD(handle)
+    streaming_context.start()
+    streaming_context.awaitTermination()
 
 
 if __name__ == '__main__':
